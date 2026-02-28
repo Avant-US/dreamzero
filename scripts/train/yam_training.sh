b836262cde0411f9e5e9d41c796cee6334209838
NValec <anagal@nvidia.com>
2026-02-28 02:46:46 +0000

    dz pretrained model on hf

--- scripts/train/yam_training.sh
+++ scripts/train/yam_training.sh
@@ -22,7 +22,7 @@ export HYDRA_FULL_ERROR=1
 YAM_DATA_ROOT=${YAM_DATA_ROOT:-"/mnt/aws-lfs-02/shared/kazheng/dreamzero/Dataset/folding_YAM"}
 
 # Output directory for training checkpoints
-OUTPUT_DIR=${OUTPUT_DIR:-"./checkpoints/dreamzero_yam_lora_dz_pretrained_100k_folding"}
+OUTPUT_DIR=${OUTPUT_DIR:-"./checkpoints/dreamzero_yam_lora_dz_pretrained_50_folding"}
 
 # Number of GPUs to use (default: all visible GPUs, so 4-GPU machines use 4 without setting NUM_GPUS)
 if [ -z "${NUM_GPUS}" ]; then
@@ -74,8 +74,8 @@ torchrun --nproc_per_node $NUM_GPUS --standalone groot/vla/experiment/experiment
     save_steps=10000 \
     training_args.warmup_ratio=0.05 \
     output_dir=$OUTPUT_DIR \
-    per_device_train_batch_size=4 \
-    max_steps=100000 \
+    per_device_train_batch_size=1 \
+    max_steps=100 \
     weight_decay=1e-5 \
     save_total_limit=10 \
     upload_checkpoints=false \
@@ -96,7 +96,6 @@ torchrun --nproc_per_node $NUM_GPUS --standalone groot/vla/experiment/experiment
     image_encoder_pretrained_path=$WAN_CKPT_DIR/models_clip_open-clip-xlm-roberta-large-vit-huge-14.pth \
     vae_pretrained_path=$WAN_CKPT_DIR/Wan2.1_VAE.pth \
     tokenizer_path=$TOKENIZER_DIR \
-    # Load pretrained weights from DreamZero
-    #pretrained_model_path=./checkpoints/dz_pretrained/copy-ckpt-50000 \
-    #++action_head_cfg.config.skip_component_loading=true \
-    #++action_head_cfg.config.defer_lora_injection=true
+    pretrained_model_path=./checkpoints/dz_pretrained \
+    ++action_head_cfg.config.skip_component_loading=true \
+    ++action_head_cfg.config.defer_lora_injection=true
